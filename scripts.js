8c474998d96bd2520f26b1ed118d461759dfd66b
Gianluca <gianlucaegallino@gmail.com>
2024-05-06 15:03:17 -0300

    Abstract player input logic to a function and validate against filled boxes

--- scripts.js
+++ scripts.js
@@ -21,7 +21,6 @@ function createCell() {
 const Game = function () {
   const player1 = createPlayer(askForName(1), 1);
   const player2 = createPlayer(askForName(2), 2);
-
   // Gameboard IIFE
   const Gameboard = function () {
     const size = 3;
@@ -47,18 +46,10 @@ const Game = function () {
   while (isRunning) {
     //Calculates player turn
     currentPlayer = turncount % 2 == 0 ? player1 : player2;
-
-    //Gets the position in which to insert.
-    console.log("Turn " + turnCount + ", " + currentPlayer.name + "'s turn.");
-    console.log("Enter row to modify");
-    let row = console.readLine();
-    console.log("Enter column to modify");
-    let col = console.readLine();
-    //Sets users input in the array
-    Gameboard.getBoard[row][col].setCellStatus(currentPlayer.value);
+    getPlay(turnCount, currentPlayer.name);
     turnCount++;
+    checkForWin(currentPlayer.value);
   }
-  // TODO: Check that plays cant be overwritter
   // TODO: Verify inputs
   // TODO: Add Win conditions.
 };
@@ -67,6 +58,26 @@ const Game = function () {
 function askForName(playerNo) {
   console.log("Enter player " + playerNo + "'s name: ");
   return console.readLine();
+}
+
+//Function for play input
+function getPlay(turn, name) {
+  let filledCorrectly = false;
+  while (!filledCorrectly) {
+    //Gets the position in which to insert.
+    console.log("Turn " + turn + ", " + name + "'s turn.");
+    console.log("Enter row to modify");
+    let row = console.readLine();
+    console.log("Enter column to modify");
+    let col = console.readLine();
+
+    //Checks if the place isnt filled.
+    if (Gameboard.getBoard[row][col].getCellStatus != 0) {
+      //Sets users input in the array
+      Gameboard.getBoard[row][col].setCellStatus(currentPlayer.value);
+      filledCorrectly = true;
+    } else console.log("That spot is already filled.");
+  }
 }
 
 // TODO: DisplayController IIFE
